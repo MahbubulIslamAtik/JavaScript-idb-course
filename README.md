88651a2f12c96d431bb94a52b4da8f2292a89fdd
MahbubulIslamAtik <atik0790@gmail.com>
2021-11-19 13:11:03 +0600

    javascript sir

---
+++ README.md
@@ -0,0 +1,1 @@
+# JavaScript-idb-course
---
+++ lettergrad_function.js
@@ -0,0 +1,18 @@
+var marks=prompt("Enter your marks : ");
+
+
+if(marks>=80){
+  document.write("You got - A+");
+}else if(marks>=70){
+  document.write("You got - A");
+}else if(marks>=60){
+  document.write("You got - A-");
+}else if(marks>=50){
+  document.write("You got - B");
+}else if(marks>=40){
+  document.write("You got - c");
+}else if(marks>=33){
+  document.write("You got - D");
+}else{
+  document.write("You are Fail");
+}
